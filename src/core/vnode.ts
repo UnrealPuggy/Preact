a5e86c1e317e6b67b28328714494050ee65e7920
UnrealPuggy <126524020+UnrealPuggy@users.noreply.github.com>
2025-12-29 19:21:55 -0700

    Idk format changes

--- src/core/vnode.ts
+++ src/core/vnode.ts
@@ -5,27 +5,25 @@ type EmptyObject = Record<PropertyKey, never>;
 export type ComponentChildren = VChild[];
 
 export type ChildrenProps = { children?: ComponentChildren };
-export type FunctionComponent<P> = (
-  props: P & ChildrenProps,
-) => VNode;
+export type FunctionComponent<P> = (props: P & ChildrenProps) => VNode;
 export type HostVNode = {
-  type: string;
+	type: string;
 
-  props: ChildrenProps;
+	props: ChildrenProps;
 };
 
 // stfu
 // deno-lint-ignore ban-types
 export type ComponentVNode<P = {}> = {
-  type: FunctionComponent<P>;
-  props: P & ChildrenProps;
+	type: FunctionComponent<P>;
+	props: P & ChildrenProps;
 };
 
-export const Fragment = Symbol("Fragment");
+export const Fragment = Symbol('Fragment');
 
 export type FragmentVNode = {
-  type: typeof Fragment;
-  props: ChildrenProps;
+	type: typeof Fragment;
+	props: ChildrenProps;
 };
 
 export type VNode = HostVNode | ComponentVNode | FragmentVNode;
--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,60 @@
-import { URL } from "node:url";
-import { h } from "./core/h.ts";
-import { ChildrenProps, Fragment } from "./core/vnode.ts";
-import { htm } from "./htm/htm.ts";
-import "./preact-version.ts";
-import { renderToString } from "./render/renderToString.ts";
-console.log("Preact Version^^^");
+import { h } from './core/h.ts';
+import { ChildrenProps, Fragment } from './core/vnode.ts';
+import { htm } from './htm/htm.ts';
+import './preact-version.ts';
+import { renderToString } from './render/renderToString.ts';
+console.log('Preact Version^^^');
 
-function MyComponent(
-  { children }: ChildrenProps,
-) {
-  console.log("ComponentChildren", children);
-  return h(Fragment, null, h("h2", null, "asd"), "Pugs are cute!");
+function MyComponent({ children }: ChildrenProps) {
+	console.log('ComponentChildren', children);
+	return h(Fragment, null, h('h2', null, 'asd'), 'Pugs are cute!');
 }
 type CommentsType = {
-  postId: number;
-  id: number;
-  name: string;
-  user: { username: string };
-  body: string;
+	postId: number;
+	id: number;
+	name: string;
+	user: { username: string; fullName: string };
+	body: string;
 }[];
+
 function ListComments({ comments }: { comments: CommentsType }) {
-  return h(
-    "ul",
-    null,
-    ...comments.map((comment) =>
-      h(
-        "li",
-        null,
-        h("strong", null, comment.name),
-        h("span", null, ` (${comment.user.username})`),
-        h("p", null, comment.body),
-      )
-    ),
-  );
+	return h(
+		'ul',
+		null,
+		...comments.map((comment) =>
+			h(
+				'li',
+				null,
+				// h('strong', null, comment.name),
+				h('span', null, comment.user.fullName),
+				h('ul', null, h('li', null, comment.body))
+			)
+		)
+	);
 }
 const comments: CommentsType = await fetch(
-  "https://dummyjson.com/comments?limit=0",
+	'https://dummyjson.com/comments?limit=0'
 )
-  .then((r) => r.json()).then((r) => r.comments);
+	.then((r) => r.json())
+	.then((r) => r.comments);
 const app = h(
-  Fragment,
-  null,
-  "Hello World!",
-  h(MyComponent, null, "asd"),
-  h(ListComments, { comments }),
+	Fragment,
+	null,
+	'Hello World!',
+	h(MyComponent, null, 'asd'),
+	h(ListComments, { comments })
 );
 const app2 = htm`hello ${MyComponent}`;
 console.log(app);
-console.log("app2", app2);
+console.log('app2', app2);
 const renderedApp = renderToString(app);
 // console.log(renderedApp);
 Deno.serve((req) => {
-  const url = new URL(req.url);
-  if (url.pathname == "/") {
-    return new Response(renderedApp, {
-      headers: { "content-type": "text/html" },
-    });
-  }
-  return new Response("404-File not found", { status: 404 });
+	const url = new URL(req.url);
+	if (url.pathname == '/') {
+		return new Response(renderedApp, {
+			headers: { 'content-type': 'text/html' },
+		});
+	}
+	return new Response('404-File not found', { status: 404 });
 });
